28aba4ee32ce908bca9998d952e20c542da7c522
Cryptassic <lkspetravicius@gmail.com>
2023-05-02 17:00:16 +0300

    feat: extend custom error

--- src/chains/cosmosV2/types/error.ts
+++ src/chains/cosmosV2/types/error.ts
@@ -1,8 +1,13 @@
 
 
 export class ProviderNotInitializedError extends Error {
-    constructor(msg?: string) {
+    innerError?: Error | unknown;   
+  
+    constructor(msg?: string, innerError?: Error | unknown) {
       super(msg);
       this.name = 'ProviderNotInitializedError';
+      this.innerError = innerError;
+
+      Object.setPrototypeOf(this, ProviderNotInitializedError.prototype);
     }
 }

78087a15b5fae017ee4118327fb7933d88dbdbee
Cryptassic <lkspetravicius@gmail.com>
2023-05-03 12:32:52 +0300

    feat: add middlewares

---
+++ src/chains/cosmosV2/cosmos-middlewares.ts
@@ -0,0 +1,32 @@
+import { CosmosV2 } from './cosmos';
+import { NextFunction, Request, Response } from 'express';
+import { isValidChain, isValidNetwork} from './cosmos.validators';
+// import { CosmosConfig } from './cosmos.config';
+
+export const verifyCosmosIsAvailable = async (
+  _req: Request,
+  _res: Response,
+  next: NextFunction
+) => {
+  const cosmos = CosmosV2.getInstance(_req.body.chain,_req.body.network);
+  if (!cosmos.ready()) {
+    await cosmos.init();
+  }
+  return next();
+};
+
+export const verifyRequestParams = async (
+  _req: Request,
+  _res: Response,
+  next: NextFunction
+) => {
+  if(!isValidChain(_req.body.chain)){
+    return next(new Error('Invalid chain'));
+  }
+  if(!isValidNetwork(_req.body.network)){
+    return next(new Error('Invalid network'));
+  }
+
+  return next();
+};
+

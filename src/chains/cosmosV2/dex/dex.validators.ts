7633b4db53cf3ff744b2d7b3ed8b3e455d747dc2
Cryptassic <lkspetravicius@gmail.com>
2023-05-13 22:50:37 +0300

    chore: formating

--- src/chains/cosmosV2/dex/dex.validators.ts
+++ src/chains/cosmosV2/dex/dex.validators.ts
@@ -1,23 +1,19 @@
-
-import { 
-    RequestValidator,
-    mkRequestValidator, 
-    Validator, 
-    mkValidator 
+import {
+  RequestValidator,
+  mkRequestValidator,
+  Validator,
+  mkValidator,
 } from '../../../services/validators';
-
 
 export const invalidPairLimitError: string =
   'The limit param is not valid. (Must be a number) and no more than 30';
 
-
 export const validateLimit: Validator = mkValidator(
-    'limit',
-    invalidPairLimitError,
-    (val) => val === undefined || typeof val === 'number' && val <= 30
+  'limit',
+  invalidPairLimitError,
+  (val) => val === undefined || (typeof val === 'number' && val <= 30)
 );
-  
 
 export const validatePairsRequest: RequestValidator = mkRequestValidator([
-    validateLimit
+  validateLimit,
 ]);

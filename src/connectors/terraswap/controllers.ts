e8faf8b89e380c97e7c833b8762919eacb5018fa
Cryptassic <lkspetravicius@gmail.com>
2023-05-14 19:36:47 +0300

    feat: add terraswap controller

---
+++ src/connectors/terraswap/controllers.ts
@@ -0,0 +1,18 @@
+import { PairInfo } from '../../chains/cosmosV2/types';
+import { TerraSwapish } from '../../services/common-interfaces';
+import { HttpException } from '../../services/error-handler';
+import {
+  UNKNOWN_ERROR_ERROR_CODE,
+} from '../../services/error-handler';
+
+export async function pairs(
+  terraswapish: TerraSwapish
+): Promise<PairInfo[]> {
+  const pairs = await terraswapish.Pairs();
+  
+  if (!pairs) {
+    throw new HttpException(500, "Failed to get pairs",UNKNOWN_ERROR_ERROR_CODE);
+  }
+
+  return pairs;
+}
